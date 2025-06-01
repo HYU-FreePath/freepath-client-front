49e0615d4e42b564455ef4d044012640723a794b
Jaehyung Lee <leejhyeong@naver.com>
2025-06-01 22:45:14 +0900

    Delete src/app/network/MapInfoAPI.ts

--- src/app/network/MapInfoAPI.ts
+++
@@ -1,6 +0,0 @@
-import { mapInfo } from '@/data/mapInfo'
-import { apiHandler } from '@/network/apiHandler'
-
-export const mapInfoAPI = async (uuid: string): Promise<mapInfo> => {
-  return apiHandler<mapInfo>(`/data/${uuid}`)
-}
