39fa51e3929e5570b420e16942753388d6586598
devITae <sky@mberd.com>
2025-06-01 21:10:13 +0900

    Add the top page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'regenerator-runtime/runtime'
 import App from './App.tsx'
+import TopPage from './TopPage.tsx'
 import './index.css'
 
 const FloorPlan = lazy(() => import('@/components/FloorPlans'))
@@ -14,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>need UUID</div>} />
+          <Route path="/" element={<TopPage />} />
           <Route path="/:uuid" element={<App />} />
-          <Route path="/floorplan/:id" element={
+          <Route path="/:uuid/floorplan/:id" element={
               <Suspense fallback={<div>Loading...</div>}>
                 <FloorPlan />
               </Suspense>
