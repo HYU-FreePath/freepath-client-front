f57bb0f7d03fe9dcce1f032dd2d615f0a757021f
devITae <sky@mberd.com>
2025-06-02 16:12:20 +0900

    Add the admin page redirection

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'regenerator-runtime/runtime'
 import App from './App.tsx'
 import TopPage from './TopPage.tsx'
+import AdminPage from './AdminPage.tsx'
 import './index.css'
 
 const FloorPlan = lazy(() => import('@/components/FloorPlans'))
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               </Suspense>
             }
           />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
