f6d5ba5297eeec7700b3351856cc68a370ec1f68
devITae <sky@mberd.com>
2025-06-03 00:55:35 +0900

    Change the admin page url

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export default function AdminPage() {
   useEffect(() => {
-    window.location.href = 'https://port-next-free-path-front-mbdezq0l7f20ef60.sel4.cloudtype.app'
+    window.location.href = 'https://port-next-free-path-front-mbdezq0l7f20ef60.sel4.cloudtype.app/login'
   }, [])
   return null
 }
